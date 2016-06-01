581ee350fe18e317ebd55006c10e415ba0177436
Savpek <savolainen.pekka@gmail.com>
2016-06-01 20:26:17 +0300

    Working version of hello world, angular2, jspm@beta

--- wwwroot/app/main.ts
+++ wwwroot/app/main.ts
@@ -1,4 +1,28 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './component'
+//import deps
+import 'reflect-metadata';
+//you may need es6-shim if you get an error relating to list.fill
+//import es6-shim;
 
-bootstrap(AppComponent); 
+import {
+  Component
+} from 'angular2/core';
+
+import { bootstrap } from 'angular2/platform/browser';
+
+//create a simple angular component
+@Component({
+  selector: 'test-app',
+  template: '<h4>Hello {{name}}</h4>'
+})
+class TestApp {
+  name: string;
+  constructor(){
+    this.name = 'Angular2';
+    setTimeout(() => {
+      this.name = 'Angular2!!!'
+    },1500);
+  }
+}
+
+//start our app
+bootstrap(TestApp);
---
+++ wwwroot/jspm.config.js
@@ -0,0 +1,69 @@
+SystemJS.config({
+  browserConfig: {
+    "paths": {
+      "github:": "/jspm_packages/github/",
+      "npm:": "/jspm_packages/npm/",
+      "kaljaa.net/": "/src/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "github:": "jspm_packages/github/",
+      "npm:": "jspm_packages/npm/",
+      "kaljaa.net/": "src/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16",
+      "os": "github:jspm/nodelibs-os@0.2.0-alpha"
+    },
+    "packages": {
+      "github:frankwallis/plugin-typescript@4.0.16": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      },
+      "github:jspm/nodelibs-os@0.2.0-alpha": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-typescript",
+  packages: {
+    "kaljaa.net": {
+      "main": "kaljaa.net.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-typescript"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {
+    "angular2": "npm:angular2@2.0.0-beta.17",
+    "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
+    "es6-shim": "github:es-shims/es6-shim@0.35.1",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "reflect-metadata": "npm:reflect-metadata@0.1.2",
+    "rxjs": "npm:rxjs@5.0.0-beta.8",
+    "zone.js": "npm:zone.js@0.6.12"
+  },
+  packages: {
+    "npm:rxjs@5.0.0-beta.8": {
+      "map": {
+        "symbol-observable": "npm:symbol-observable@0.2.4"
+      }
+    }
+  }
+});
